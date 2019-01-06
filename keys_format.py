7d1adaa5050d56fcae7d3edd742cbc4d64aaddd8
Otmane Chaibe <oc35@njit.edu>
2019-01-06 14:40:47 -0500

    Create keys_format.py

---
+++ keys_format.py
@@ -0,0 +1,7 @@
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'AAAAAA'
+CONSUMER_SECRET = 'BBBBBB'
+ACCESS_KEY = 'CCCCCC'
+ACCESS_SECRET = 'DDDDDD'
